3d5c547dc08a56b2df9dff481acba4e7a8f9c420
SnowZhang <zhang0215>
2016-05-24 11:02:09 +0800

    呵呵呵

--- app/src/main/java/com/snowzhang/myapplication/MainActivity.java
+++ app/src/main/java/com/snowzhang/myapplication/MainActivity.java
@@ -2,12 +2,31 @@ package com.snowzhang.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.id_btn);
+
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.id_btn:
+                Toast.makeText(this,"click",Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
     }
 }
